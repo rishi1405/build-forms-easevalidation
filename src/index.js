860d233e69eda52b23534ab44afbd5d7f9dab0b4
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 18:09:58 +0530

    ReactJS7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,130 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const EmployeeComponent =()=> {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+
+    <Formik initialValues= {{
+
+      Id: '',
+
+      Name: '',
+
+      Location: '',
+
+      Salary:'',
+
+      EmailId:'',
+
+      Designation:''
+
+    }} validationSchema={yup.object({
+
+      Name:yup.string().max(20,'Name should not exceed 20 Characters').
+
+      required('Please Enter Employee Name'),
+
+      Location: yup.string()
+
+        .required('Please Enter Employee Location'),
+
+      EmailId: yup.string()
+
+        .email('Invalid email address')
+
+        .required('Please Enter Email Id')
+
+    })} onSubmit= {values => {
+
+      alert(JSON.stringify(values));
+
+    }}>
+
+      {props=>(
+
+        <div>
+
+    <h2>Enter Employee Details...</h2>
+
+    <Form>
+
+      <p>
+
+          <label htmlFor="Id">Employee ID </label>
+
+          <Field name="Id" type="text"></Field>
+
+      </p>
+
+      <p>
+
+          <label htmlFor="Name">Employee Name </label>
+
+          <Field name="Name" type="text"></Field>
+
+          <ErrorMessage name="Name"></ErrorMessage>
+
+      </p>
+
+      <p>
+
+          <label htmlFor="Location">Employee Location </label>
+
+          <Field name="Location" type="text"></Field>
+
+          <ErrorMessage name="Location"></ErrorMessage>
+
+      </p>
+
+      <p>
+
+          <label htmlFor="Salary">Employee Salary </label>
+
+          <Field name="Salary" type="text"></Field>
+
+      </p>
+
+      <p>
+
+          <label htmlFor="EmailId">Employee Email ID </label>
+
+          <Field name="EmailId" type="text"></Field>
+
+          <ErrorMessage name="EmailId"></ErrorMessage>
+
+      </p>
+
+      <p>
+
+        <label>Employee Designation : 
+
+        <Field name="Designation" as="select">
+
+  <option value="red">Software Engineer</option>
+
+  <option value="green">Senior Software Engineer</option>
+
+  <option value="blue">Lead</option>
+
+</Field>
+
+        </label>
+      </p>
+      <button type="submit" disabled={props.isValid==false}>Submit</button>
+
+    </Form>
+
+    </div>
+
+      )}
+
+    </Formik>
+  );
+}
+
+const element=<EmployeeComponent></EmployeeComponent>
+
+ReactDOM.render(element,document.getElementById("root"));
